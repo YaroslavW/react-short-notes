fecc765c30fc843048a2bf514e045686fad00a09
YaroslavW <doctor355@yahoo.com>
2019-03-10 15:49:29 +0300

    React Scroll-to top as separete component-layout

---
+++ scroll-to-top/ScrollButton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ScrollButton extends React.Component {
+    constructor() {
+      super();
+  
+      this.state = {
+          intervalId: 0
+      };
+    }
+    
+    scrollStep() {
+      if (window.pageYOffset === 0) {
+          clearInterval(this.state.intervalId);
+      }
+      window.scroll(0, window.pageYOffset - this.props.scrollStepInPx);
+    }
+    
+    scrollToTop() {
+      let intervalId = setInterval(this.scrollStep.bind(this), this.props.delayInMs);
+      this.setState({ intervalId: intervalId });
+    }
+    
+    render () {
+        return <button title='Back to top' className='scroll' 
+                 onClick={ () => { this.scrollToTop(); }}>
+                  <span className='arrow-up glyphicon glyphicon-chevron-up'></span>
+                </button>;
+     }
+  } 
+
+ScrollButton.propTypes = {
+    scrollStepInPx: PropTypes.string.isRequired,
+    delayInMs: PropTypes.string.isRequired
+}
+  
+export default ScrollButton;
