62cc59b10d60fa6e2de9e17788a8a9f2fb00c2c4
Yaroslav <doctor355@yahoo.com>
2019-04-17 14:05:45 +0300

    Add tick with running and change state every second

--- Modern- React/Asynchrony/index.js
+++ Modern- React/Asynchrony/index.js
@@ -9,15 +9,33 @@ const demoData = Array.from({
   id: i,
   name: Math.random()
 }));
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <List data={demoData} />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    list: demoData
+  }
+  tick() {
+    const newList = [...this.state.list];
+    newList.forEach(item => {
+      item.name++;
+    });
+    this.setState({
+      list: newList
+    })
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <List data={this.state.list} />
+      </div>
+    );
+  }
 }
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
