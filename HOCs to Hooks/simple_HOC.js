73f16a56a38cc502c38d5d2689def9cbe1f4a361
Yaroslav <doctor355@yahoo.com>
2019-05-28 22:19:05 +0300

    HOC to Hooks simple HOC for static data

--- HOCs to Hooks/simple_HOC.js
+++ HOCs to Hooks/simple_HOC.js
@@ -3,3 +3,10 @@
  
 //   return <Comp ...{hookData} {...props} />;
 // }
+
+
+// If your hook implementation requires static data, you can pass it in as an argument:
+const withMyHook = hookArgs => Comp => () => {
+  const hookData = useMyHook(hookArgs);
+  return <Comp {...hookData} {...props} />;
+};
