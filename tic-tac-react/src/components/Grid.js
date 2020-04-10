7169edcfefb5b1fa449cbcff601d3d7bfec938e1
Yaroslav <doctor355@yahoo.com>
2020-04-10 09:21:00 +0300

    clear style App.css and 3 blocks with Square added

--- tic-tac-react/src/components/Grid.js
+++ tic-tac-react/src/components/Grid.js
@@ -10,14 +10,24 @@ const Grid = () => {
   return (
     <div>
       <h1>Title</h1>
-      <div>
-        {gridValue[0].map(block=>{
-          return <Square/>
+      <div style={{ display: "flex" }}>
+        {gridValue[0].map((block) => {
+          return <Square />;
+        })}
+      </div>
+      <div style={{ display: "flex" }}>
+        {gridValue[0].map((block) => {
+          return <Square />;
+        })}
+      </div>
+      <div style={{ display: "flex" }}>
+        {gridValue[0].map((block) => {
+          return <Square />;
         })}
       </div>
       <h4>Total</h4>
     </div>
-  )
+  );
 }
 
 export default Grid;
